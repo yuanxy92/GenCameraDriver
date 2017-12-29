15bf819d99461e41c151bbeb2e07417a8fe470ed
ShaneYuan <yuanxy92@gmail.com>
2017-12-29 16:26:47 +0800

    add files

---
+++ GenCameraDriver.hpp
@@ -0,0 +1,18 @@
+/**
+@brief General Camera Driver Class
+@author Shane Yuan
+@date Dec 29, 2017
+*/
+
+class GenCamera {
+private:
+
+public:
+
+private:
+
+public:
+    GenCamera() {}
+    ~GenCamera() {}
+
+}
