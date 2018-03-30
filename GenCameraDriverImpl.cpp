6bb419a80f4567fde83856abcf660ec52333703e
ShaneYuan <yuanxy92@gmail.com>
2018-03-30 11:36:38 +0800

    add

--- GenCameraDriverImpl.cpp
+++ GenCameraDriverImpl.cpp
@@ -14,7 +14,7 @@ namespace cam {
 	@breif function to init camera array
 	@return
 	*/
-	std::shared_ptr<GenCamera> createCamera(CameraModel camModel, std::string dir = "") {
+	std::shared_ptr<GenCamera> createCamera(CameraModel camModel, std::string dir) {
 		if (camModel == CameraModel::XIMEA_xiC) {
 			std::shared_ptr<GenCameraXIMEA> cameraPtr = std::make_shared<GenCameraXIMEA>();
 			return std::static_pointer_cast<GenCamera>(cameraPtr);
