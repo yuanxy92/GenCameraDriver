5bde22e9c9f559ae5df37f4dcedb419c9071a7f4
yuanxy92 <yuanxy92@gmail.com>
2018-08-01 20:46:08 +0800

    add

--- Tools/download.sh
+++ Tools/download.sh
@@ -3,4 +3,4 @@
 # argv2 keyname
 # argv3 server dir
 # argv4 local dir
-scp -i $2 -r zhu-ty@$1:/home/zhu-ty/Projects/GenCameraDriver/build/$3 $4
+scp -i $2 -r zhu-ty@$1:/home/zhu-ty/Projects/shell/$3 $4
