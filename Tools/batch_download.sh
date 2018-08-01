8a839418237a31ecc5365503a7e70ae359ebde95
yuanxy92 <yuanxy92@gmail.com>
2018-08-01 21:31:26 +0800

    modify out dir

--- Tools/batch_download.sh
+++ Tools/batch_download.sh
@@ -1,2 +1,2 @@
-sh download.sh 10.8.3.231 key231 out/1 ./videos/1
-sh download.sh 10.8.3.232 key232 out/1 ./videos/2
+sh download.sh 10.8.3.231 key231 out ./videos/
+sh download.sh 10.8.3.232 key232 out ./videos/
