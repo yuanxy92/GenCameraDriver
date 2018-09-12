12ef4e484a1c154aa126bfcbe095987c9fac325c
Hunter <yuanxy92@163.com>
2018-09-12 18:01:10 +0800

    only serialnum is needed for decoding

--- Tools/Decoder/main.cpp
+++ Tools/Decoder/main.cpp
@@ -1,3 +1,10 @@
+#ifdef _WIN32
+#include <io.h> 
+#define access    _access_s
+#else
+#include <unistd.h>
+#endif
+
 // include std
 #include <cstdio>
 #include <cstdlib>
@@ -19,9 +26,29 @@
 
 #include "NPPJpegCoder.h"
 
+inline bool isFileExists(const std::string &Filename) {
+	return access(Filename.c_str(), 0) == 0;
+}
+
 int main(int argc, char* argv[]) {
-	std::string input(argv[1]);
-	std::string output(argv[2]);
+	std::string input_dir(argv[1]);
+	std::string serialnum(argv[2]);
+	std::string output(argv[3]);
+	std::string videoname;
+
+	// get exist filenames
+	cv::String path = input_dir;
+	std::vector<cv::String> dirFiles;
+	cv::glob(path, dirFiles);
+	for (size_t k = 0; k < dirFiles.size(); k++) {
+		std::size_t found = dirFiles[k].find(serialnum);
+		if (found != std::string::npos) {
+			videoname = dirFiles[k];
+			break;
+		}
+	}
+
+	std::cout << cv::format("Find bin file: %s", videoname.c_str()) << std::endl;
 
 	int frameNum;
 	int quality;
@@ -31,7 +58,7 @@ int main(int argc, char* argv[]) {
 	cv::VideoWriter writer;
 	npp::NPPJpegCoder coder;
 
-	FILE* fp = fopen(input.c_str(), "rb");
+	FILE* fp = fopen(videoname.c_str(), "rb");
 
 	fread(&frameNum, sizeof(unsigned int), 1, fp);
 	fread(&width, sizeof(int), 1, fp);
--- Tools/batch_download.sh
+++ Tools/batch_download.sh
@@ -1,2 +1,2 @@
-sh download.sh 10.8.3.231 key231 out ./videos/
-sh download.sh 10.8.3.232 key232 out ./videos/
+sh download.sh 10.8.5.10 key231 out ./videos/
+sh download.sh 10.8.5.12 key232 out ./videos/
