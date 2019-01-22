fc7b3c358d51b051d6db92226758edd7dcdf064a
zhu-ty <zhu-ty@outlook.com>
2019-01-22 19:48:24 +0800

    added option rate

--- SKEncoder/H265Convert.sh
+++ SKEncoder/H265Convert.sh
@@ -1,8 +1,13 @@
 dir="."
+rate=30
 if [ $# -eq 0 ];then
     echo "No arguments supplied, will use [where I am] dir"
 else
     dir=$1
+fi
+
+if [ $# -gt 1 ];then
+    rate=$2
 fi
 
 for i in `ls $dir/*.h265`
@@ -11,7 +16,7 @@ do
     filename=${i##*/}
     #echo $filename
     #echo ${filename%.*}
-    ffmpeg -y -i $i -vcodec copy $dir/${filename%.*}.mp4
+    ffmpeg -y -r ${rate} -i $i -vcodec copy $dir/${filename%.*}.mp4
 done
 
 #echo $dir
